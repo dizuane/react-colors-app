a5d219c29073bbe9b0c69b972817e91ca88f055c
Duane Spainhour <duane.spainhour@gmail.com>
2020-05-12 03:06:28 -0400

    Added color slider

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import seedPalettes from './seedPalettes';
 import { generatePalette } from './colorHelpers';
 
 function App() {
-  console.log(generatePalette(seedPalettes[4]));
   return (
     <div className="App">
-      <Palette {...seedPalettes[4]} />
+      <Palette palette={generatePalette(seedPalettes[4])} />
     </div>
   );
 }
