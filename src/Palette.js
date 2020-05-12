6ad38ba08b67ee5de64c0307a7c2b9505fc6e863
Duane Spainhour <duane.spainhour@gmail.com>
2020-05-12 03:34:14 -0400

    Added navbar

--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import Slider from 'rc-slider';
-import 'rc-slider/assets/index.css';
 
 import ColorBox from './ColorBox';
+import Navbar from './Navbar';
 
 import './Palette.css';
 
@@ -26,15 +25,7 @@ class Palette extends Component {
         ));
         return (
             <div className="Palette">
-                <div className="slider">
-                    <Slider
-                        defaultValue={level}
-                        min={100}
-                        max={900}
-                        step={100}
-                        onAfterChange={this.changeLevel}
-                    />
-                </div>
+                <Navbar level={level} changeLevel={this.changeLevel} />
                 <div className="Palette-colors">
                     {colorBoxes}
                 </div>
