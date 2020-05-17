22e31ee197de1587fdd4faf6dcb9a5e3489bddb0
Duane Spainhour <duane.spainhour@gmail.com>
2020-05-16 23:23:05 -0400

    Made colorbox and palette responsive

--- src/styles/sizes.js
+++ src/styles/sizes.js
@@ -7,7 +7,7 @@ export default {
             xs: "575.98px",
             sm: "767.98px",
             md: "991.98px",
-            lg: "119.98px"
+            lg: "1199.98px"
         }
 
         return `@media (max-width: ${sizes[size]})`;
